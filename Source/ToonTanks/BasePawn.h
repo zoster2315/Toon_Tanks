412a996d56f9fd7525cbadd6c5b4dd4cdfa29852
zoster2315 <zoster2315@gmail.com>
2022-04-17 15:08:34 +0400

    Базовый Pawn класс. Наследники для танка и пушек. Компоненты в базовом классе, управление их доступностью в редакторе и блупринтах.

--- Source/ToonTanks/BasePawn.h
+++ Source/ToonTanks/BasePawn.h
@@ -9,21 +9,34 @@
 UCLASS()
 class TOONTANKS_API ABasePawn : public APawn
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	ABasePawn();
+    // Sets default values for this pawn's properties
+    ABasePawn();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+        class UCapsuleComponent* CapsuleComp;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+        UStaticMeshComponent* BaseMesh;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+        UStaticMeshComponent* TurretMesh;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+        USceneComponent* ProjectileSpawnPoint;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"), Category = "Some Category")
+        int32 EditAnywhereInt = 12;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
